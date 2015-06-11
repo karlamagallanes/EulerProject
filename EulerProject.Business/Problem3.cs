97a538b6a3d625057d1a9e0528934030883cebeb
Karla Magallanes <karlaimagallanes@gmail.com>
2015-06-11 14:04:47 -0700

    Fixing Compilation Error..
    Was sleepy on last commit. -.-

--- EulerProject.Business/Problem3.cs
+++ EulerProject.Business/Problem3.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EulerProject.Business
 {
-    public class Problem3:Problem
+    public class Problem3 : Problem
     {
         /// <summary>
         /// The prime factors of 13195 are 5, 7, 13 and 29.
@@ -14,7 +14,7 @@ namespace EulerProject.Business
         /// </summary>
         public override void Execute()
         {
-            Int64 max=600851475143;
+            Int64 max = 600851475143;
             Console.WriteLine(string.Format("Result: {0}", GetResult(max)));
         }
 
@@ -22,15 +22,15 @@ namespace EulerProject.Business
         private static Int64 GetResult(Int64 max)
         {
             List<Int64> primes = new List<Int64>();
-            
-            
-            
-           //TODO: primes must be ordered.
+
+
+
+            //TODO: primes must be ordered.
             return primes.LastOrDefault();
         }
     }
 
-        }
-
-    }
 }
+
+
+
