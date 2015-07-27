2731a434eedfc422013b7ca500c42ffbc11d85c5
Karla Magallanes <karlaimagallanes@gmail.com>
2015-07-27 14:48:00 -0700

    Problem 4 Solved
    Añora la Roña..

--- EulerProject.Business/Problem4.cs
+++ EulerProject.Business/Problem4.cs
@@ -1,16 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EulerProject.Business
 {
-    class Problem4:IProblem
+    public class Problem4 : IProblem
     {
+        /// <summary>
+        /// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+        /// Find the largest palindrome made from the product of two 3-digit numbers.
+        /// </summary>
         public string Execute()
         {
-            throw new NotImplementedException();
+            return GetResult().ToString();
+        }
+
+        private int GetResult()
+        {
+            int result = 0;
+            int maxResult = 0;
+            for (int x=999; x > 0; x--)
+                for (int y = x; y > 0; y--)
+                {
+                    result = x * y;
+                    if (IsPalindrome(result.ToString()) && result > maxResult)
+                        maxResult = result;
+                }
+            return maxResult;
+        }
+
+        private bool IsPalindrome(string result)
+        {
+            char[] c = result.ToCharArray();
+            int x = result.Length - 1;
+            for (int i = 0; i < x; i++, x--)
+            {
+                if (c[i] != c[x])
+                    return false;
+            }
+            return true;
         }
     }
 }
